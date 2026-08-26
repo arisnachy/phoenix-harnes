e1494dabb4f53bf6a0b288acc87d4b0143407f0c
arisnachy <127772990+arisnachy@users.noreply.github.com>
2026-08-25 20:27:29 -0400

    feat(ui): let Settings shell render trailing status

--- packages/client/ui-settings-general/src/client/shell-contract.ts
+++ packages/client/ui-settings-general/src/client/shell-contract.ts
@@ -50,6 +50,7 @@ export type SettingsRootComponentProps =
   PropsRuntime<'sidebar.settings'>
   & PropsRenderSlots<
     | 'settings.trigger'
+    | 'settings.trigger.trailing'
     | 'settings.header'
     | 'settings.action'
     | 'settings.close'
